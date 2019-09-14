02781e59cee3f7f807e931647b29a00697e76a93
Abhishek Sirohi <32898073+sirohiwebdev@users.noreply.github.com>
2019-09-14 15:49:36 +0530

    Completed Internship Assignment
    Done Delivery Personnel Manager Assignment

---
+++ src/delivery_personnel_data.js
@@ -0,0 +1,34 @@
+const persons = [
+  {
+    name: "John Mike",
+    id: 1,
+    status: null
+  },
+  {
+    name: "Brad Smith",
+    id: 2,
+    status: null
+  },
+  {
+    name: "Stephnie",
+    id: 3,
+    status: null
+  },
+  {
+    name: "Steve",
+    id: 4,
+    status: null
+  },
+  {
+    name: "Tony",
+    id: 5,
+    status: null
+  },
+  {
+    name: "Ravi",
+    id: 6,
+    status: null
+  }
+];
+
+export default persons;
